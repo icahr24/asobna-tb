d3bd674229afc8aa19260e269b4626eef9dd240e
icahr24 <167745349+icahr24@users.noreply.github.com>
2024-04-25 18:49:01 +0200

    nedostaje češća provjera button B

--- main.ts
+++ main.ts
@@ -1,21 +1,29 @@
 function mjerenje () {
     if (m == 1) {
-        celzius = input.temperature()
+        celzius = Math.floor(input.temperature() / 10)
         basic.showNumber(celzius)
-        if (m != 1) {
-            basic.clearScreen()
-        }
+        basic.pause(200)
+        celjed = input.temperature() - celzius * 10
+        basic.showNumber(celjed)
+        basic.pause(1000)
+        basic.clearScreen()
+    } else {
+        basic.clearScreen()
     }
 }
+let celjed = 0
 let celzius = 0
 let m = 0
 basic.clearScreen()
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         m = 1
+        mjerenje()
     }
     if (input.buttonIsPressed(Button.B)) {
         m = 0
+        basic.clearScreen()
+    } else {
+        mjerenje()
     }
-    mjerenje()
 })
